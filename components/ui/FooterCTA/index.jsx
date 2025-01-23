4e3bb0ccb48212bbd4c96fd2e6331cfbc9fe9b8e
quentin <quentinderycke6@gmail.com>
2025-01-23 15:40:02 +0100

    update testimonials

--- components/ui/FooterCTA/index.jsx
+++ components/ui/FooterCTA/index.jsx
@@ -5,18 +5,16 @@ const FooterCTA = () => (
     <SectionWrapper>
         <div className="custom-screen">
             <div className="max-w-2xl mx-auto text-center">
-                <h2 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
-                    Get started with Blinder today
-                </h2>
+                <h2 className="text-blue-500 text-3xl font-semibold sm:text-4xl">
+                Pourquoi faire appel à nous ?                </h2>
                 <p className="mt-3 text-gray-600">
-                    Hire experts to create your next idea, follow best practices, remove roadblocks, and delivery on schedule.
-                </p>
+                Engagez des experts juridiques pour sécuriser vos projets, anticiper les risques et défendre vos intérêts avec efficacité.                </p>
                 <NavLink
                     href="/get-started"
                     className="mt-4 inline-block font-medium text-sm text-white bg-gray-800 hover:bg-gray-600 active:bg-gray-900"
                 >
-                    Start building
-                </NavLink>
+Contactez-nous maintenant
+</NavLink>
             </div>
         </div>
     </SectionWrapper>
